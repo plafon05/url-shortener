b65e9075ad404fbe8b7b1aad66f2e4ff73257c6c
Данила Желтов <dzheltov4@gmail.com>
2025-11-18 13:50:16 +0300

    Инициализировать проект url-shortener: структура, конфиг и main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: pastgreSQL
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+}
