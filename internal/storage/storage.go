3a9491bab96ea8c38c7235e6cfda80e53df4bae2
Данила Желтов <dzheltov4@gmail.com>
2025-11-30 00:05:46 +0300

    feat(storage): добавить ErrAliasNotFound и обновить зависимости

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import "errors"
 
 var (
-	ErrURLNotFound = errors.New("url not found")
-	ErrURLExists   = errors.New("url already exists")
+	ErrURLNotFound   = errors.New("url not found")
+	ErrAliasNotFound = errors.New("alias not found")
+	ErrURLExists     = errors.New("url already exists")
 )
