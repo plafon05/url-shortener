571c96a373193d28de8a3dd2a06d0b41feaab1d4
Данила Желтов <dzheltov4@gmail.com>
2025-11-29 23:48:28 +0300

    chore(http-server): настроен router с middleware
    - Добавлены chi middleware: RequestID, RealIP, Recoverer, URLFormat
    - Подключен кастомный middleware для логирования

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"httpServer_project/internal/config"
+	mwLogger "httpServer_project/internal/http-server/middleware/logger"
 	"httpServer_project/internal/storage/postgres"
+	"httpServer_project/lib/logger/handlers/slogpretty"
 	"httpServer_project/lib/logger/slg"
 	"log/slog"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -15,35 +20,42 @@ const (
 )
 
 func main() {
+
 	cfg := config.MastLoad()
 
 	log := setupLogger(cfg.Env)
 	log.Info("start url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug massages are enabled")
 
+	// Инициализирую хранилище моих данных
 	storage, err := postgres.New(cfg.Postgres.Dsn)
+
 	if err != nil {
 		log.Error("failed to initialize storage", slg.Err(err))
 		os.Exit(1)
 	}
 
-	defer storage.Close() //// Закрываем пул соединений при завершении программы
+	defer storage.Close() // Закрываю пул соединений при завершении программы
 
-	_ = storage
+	// Иницилизация роутера на go-chi
+	router := chi.NewRouter()
 
-	// TODO: init router: chi, "chi render"
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
 	// TODO: run server
 }
 
+// Настройка логгера в зависимости от окружения
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -55,4 +67,17 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+// Настройка pretty slog логгера
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
